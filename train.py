ef693a857c09ec9773bbbaa4760d61bfed0ccfe8
Akash Shingha <akashshingha850@gmail.com>
2026-02-17 14:57:41 +0200

    Refactor training script to support multiple model variants and improve readability

--- train.py
+++ train.py
@@ -1,18 +1,30 @@
 from ultralytics import YOLO
 import os
 import wandb
+
+EPOCHS = 100
+IMGSZ = 640
+BATCH_SIZE = 8
+PATIENCE = 20
+
+YOLO_MODEL = "yolo26"  # Pretrained model to start from
+VARIENT = ['n', 's', 'm', 'l', 'x']
+
+#clear terminal
 os.system('clear')
 
-MODEL_NAME = "yolo26m"
-EPOCHS = 100
-BATCH_SIZE = 8
-CLOSE_MOSAIC = 10
-PATIENCE = 10
+# wandb.init(project="polesight")
 
+# model = YOLO(f"{YOLO_MODEL}-seg.pt")  # load a pretrained model (recommended for training)
 
-model = YOLO(f"{MODEL_NAME}-seg.pt")  # load a pretrained model (recommended for training)
-
-wandb.init(project="polesight", name=f"{MODEL_NAME}_training")
-
-
-results = model.train(data="data/data.yaml", epochs=EPOCHS, imgsz=640, batch=BATCH_SIZE, close_mosaic=CLOSE_MOSAIC, patience=PATIENCE, save=True)
+for var in VARIENT:
+    print(f"Training {YOLO_MODEL}{var}-seg.pt")
+    model = YOLO(f"{YOLO_MODEL}{var}-seg.pt")  # load a pretrained model (recommended for training)
+    # Let YOLO handle wandb initialization internally
+    results = model.train(data="data/data.yaml", 
+                          project="polesight",
+                          name=f"{YOLO_MODEL}{var}-seg", 
+                          epochs=EPOCHS, imgsz=IMGSZ, 
+                          batch=BATCH_SIZE, patience=PATIENCE,
+                          save = True,  # Save the best model
+                          )
