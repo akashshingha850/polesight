cd633ed289172b6fa2179b4771d7f8dd1ec5022e
Akash Shingha <akashshingha850@gmail.com>
2026-05-27 14:52:17 +0300

    add vt7 data

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 import os
 import wandb
 
-EPOCHS = 10
+EPOCHS = 100
 IMGSZ = 640
 BATCH_SIZE = 16
 PATIENCE = 20
@@ -28,12 +28,31 @@ for var in VARIENT:
             model = YOLO(f"{YOLO_MODEL}{var}-seg.pt")  # load a pretrained model (recommended for training)
    
             # Let YOLO handle wandb initialization internally
-            results = model.train(data="data/data.yaml", 
+            results = model.train(data="data/data.yaml",
                                   project="polesight",
-                                  name=f"{YOLO_MODEL}{var}-{BATCH}_light_pole",  # Unique name for this run
-                                  epochs=EPOCHS, imgsz=IMGSZ, 
+                                  name=f"{YOLO_MODEL}{var}-{BATCH}",  # Unique name for this run
+                                  epochs=EPOCHS, imgsz=IMGSZ,
                                   batch=BATCH, patience=PATIENCE,
-                                  save = True,  # Save the best model
+                                  save=True,
+                                  classes=[0, 1, 2, 4],
+                                  # Augmentation disabled
+                                  hsv_h=0.0,
+                                  hsv_s=0.0,
+                                  hsv_v=0.0,
+                                  degrees=0.0,
+                                  translate=0.0,
+                                  scale=0.0,
+                                  shear=0.0,
+                                  perspective=0.0,
+                                  flipud=0.0,
+                                  fliplr=0.0,
+                                  bgr=0.0,
+                                  mosaic=0.0,
+                                  mixup=0.0,
+                                  copy_paste=0.0,
+                                  auto_augment=False,
+                                  erasing=0.0,
+                                  crop_fraction=1.0,
                                   )
             break  # Training successful
         except RuntimeError as e:
